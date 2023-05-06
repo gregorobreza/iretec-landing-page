09da560d7c1110446d0836ff959303eac166e38a
Gregor Obreza <gobreza@gmail.com>
2023-05-06 18:21:55 +0200

    added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,19 @@
-import { CompanySection } from '@/components/sections/companySection'
-import { HeroSection } from '@/components/sections/heroSection'
-import { ProductSection } from '@/components/sections/productSection'
-import { ServiceSection } from '@/components/sections/serviceSection'
-import Image from 'next/image'
+import { CompanySection } from "@/components/sections/companySection";
+import { Contact } from "@/components/sections/contact";
+import { HeroSection } from "@/components/sections/heroSection";
+import { ProductSection } from "@/components/sections/productSection";
+import { ServiceSection } from "@/components/sections/serviceSection";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main >
-    {/* <main className="flex min-h-screen flex-col items-center justify-between p-24"> */}
-       <HeroSection/>
-       <CompanySection/>
-       <ServiceSection/>
-       <ProductSection/>
+    <main>
+      {/* <main className="flex min-h-screen flex-col items-center justify-between p-24"> */}
+      <HeroSection />
+      <CompanySection />
+      <ServiceSection />
+      <ProductSection />
+      {/* <Contact /> */}
       {/* <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
@@ -118,5 +120,5 @@ export default function Home() {
         </a>
       </div> */}
     </main>
-  )
+  );
 }
---
+++ src/components/sections/contact.tsx
@@ -0,0 +1,30 @@
+export function Contact() {
+  return (
+    <div className="flex">
+      <div className="flex flex-col">
+        <h2 className="text-3xl text-center  font-bold tracking-tight text-gray-900 sm:text-5xl">
+          Let&apos;s talk
+        </h2>
+        <div className="text-dark mb-3 text-2xl font-semibold">
+          Damjan Zadnik, Founder and CEO
+        </div>
+        <a
+          href="#"
+          className="-mx-3 block rounded-lg px-3 py-2 text-lg font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+        >
+          damjan.zadnik@iretec.eu
+        </a>
+      </div>
+      <div>
+        <div>
+          <div>Iretec d.o.o.</div>
+          <div>
+            <div>Vodiška cesta 71</div>
+            <div>1217 Vodice</div>
+            <div>Slovenia</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
